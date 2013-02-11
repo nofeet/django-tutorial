11921f721e523e25cc64e6209ed2812916d599ec
Greg Nofi <greg.nofi@getglue.com>
2013-02-11 16:31:41 -0500

    Completed part 4 of the tutorial.

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,5 +1,10 @@
 # Django settings for mysite project.
 
+import os.path
+
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -12,7 +17,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/gnofi/workspace/mysite/sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(PROJECT_ROOT, 'sqlite.db'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -106,7 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/gnofi/django_templates',
+    os.path.join(PROJECT_ROOT, '../polls/templates'),
 )
 
 INSTALLED_APPS = (
